b2570d653d4a3a21f2e9e2ed467effe393e51ab8
eivmosn <eivmosn@163.com>
2024-01-07 23:29:36 +0800

    chore: naive-ui update to 2.37.0

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,6 @@ export default defineComponent({
         <div class="p-4 h-full base-content">
           <div class="">文字</div>
           <div class="sub-text">描述</div>
-          {/* <div class="h-40px p-4 sub-content base-border-full">
-
-          </div> */}
         </div>
       </Provider>
     )
