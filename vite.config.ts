752c7b90af4e02dd15ee5b8a088c62f48d8d3b23
eivmosn <eivmosn@163.com>
2024-08-19 18:18:05 +0800

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
-import Vue from '@vitejs/plugin-vue'
 import VueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
   plugins: [
-    Vue(),
     VueJsx(),
     UnoCSS(),
   ],
