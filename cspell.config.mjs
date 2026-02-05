eb4641fe77c147f24861f560d8a0f290255153ad
eivmosn <eivmosn@163.com>
2026-02-05 23:49:36 +0800

    feat: add cspell checker for code

---
+++ cspell.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'cspell'
+
+export default defineConfig({
+  version: '0.2',
+  language: 'en',
+  words: [
+    'vueuse',
+  ],
+  ignorePaths: [
+    'node_modules/**',
+    'public/**',
+  ],
+  files: [
+    'src/**/*.{vue,ts,tsx,css}',
+  ],
+})
